b6286ed7a035921a3eb54528a6041532ec3b5712
Yogi Indragiri <g13.indra@gmail.com>
2020-08-26 22:26:15 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/hallo', function () {
+    return "Hallo Yogi :)";
+});
+
 
 Route::get('/status', function () {
     $dbstatus = "";
